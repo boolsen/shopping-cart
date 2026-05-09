08cf703cf8258cbc833faad6bdd8d2b514553650
Audun Kronstad <audunkronstad@Audun-sin-MacBook-Air.local>
2026-05-09 20:03:02 +0200

    working on shopping cart

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,5 +1,22 @@
-function Content() {
-    return ( <></> );
+import Card from "../Card/Card.jsx"
+import "./Content.css"
+
+function Content({ products, selectedCategory }) {
+    console.log(products, 'test');
+
+    if (!products) {
+        return <div className="loading">Loading products...</div>;
+    }
+    
+    return (
+        <div className="products">
+            {
+                products?.map((product) => {
+                    return <Card key={product.name} product={product}/>
+                })
+            }
+        </div>
+    );
 }
 
 export default Content;
